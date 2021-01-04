822ca1792f1f60c845dd0275474b93440f794029
DePrincio <deprincio@DePrincio.local>
2021-01-04 12:30:53 -0500

    commit

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Head() {
+  return (
+    <div>
+      <header>
+        <h1>Corona Virus Tracking App</h1>
+      </header>
+    </div>
+  );
+}
