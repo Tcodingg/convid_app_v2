74c827a8adb60e9e206895d2826f7e664b502dd5
DePrincio <deprincio@DePrincio.local>
2021-01-10 17:24:47 -0500

    add sort to every column

--- src/table/Table.jsx
+++ src/table/Table.jsx
@@ -2,51 +2,92 @@ import React, { useState, useEffect } from 'react';
 import { fetchedByCountries } from './api';
 
 export default function Table() {
-  const [result, setResult] = useState('');
-  const [country, setCountry] = useState('United Kingdom');
-
-  const [active, setActive] = useState('');
-  const [confirmed, setConfirmed] = useState('');
-  const [recovered, setRecovered] = useState('');
-  const [deaths, setDeaths] = useState('');
-
+  const [result, setResult] = useState([]);
+  const [country, setCountry] = useState('');
+  const [sortCol, setSortCol] = useState('descending');
+  const [column, setColumn] = useState('');
   useEffect(() => {
     const fetchedData = async () => {
       const { data } = await fetchedByCountries();
-      setResult(data);
+
+      // setResult(data);
+      setResult(() =>
+        data.filter((region) => region.countryRegion === 'United Kingdom')
+      );
     };
     fetchedData();
   }, []);
-
-  if (!result) {
-    return <div></div>;
+  function sortField(key) {
+    if (sortCol === 'ascending') {
+      const aso = [...result].sort((a, b) => {
+        return b[key] - a[key];
+      });
+      setResult(aso);
+      setSortCol('descending');
+    } else if (sortCol === 'descending') {
+      const dso = [...result].sort((a, b) => {
+        return a[key] - b[key];
+      });
+      setResult(dso);
+      setSortCol('ascending');
+    }
   }
-
   return (
     <table>
       <thead>
         <tr>
           <th>Province/State</th>
-
-          <th>Active</th>
-          <th>Confirmed</th>
-          <th>Recovered</th>
-          <th>Deaths</th>
+          <th>
+            <button
+              onClick={() => {
+                sortField('active');
+              }}
+            >
+              Active
+            </button>
+          </th>
+          <th>
+            <button
+              onClick={() => {
+                sortField('confirmed');
+              }}
+            >
+              Confirmed
+            </button>
+          </th>
+          <th>
+            <button
+              onClick={() => {
+                sortField('recovered');
+              }}
+            >
+              Recovered
+            </button>
+          </th>
+          <th>
+            <button
+              onClick={() => {
+                sortField('deaths');
+              }}
+            >
+              Deaths
+            </button>
+          </th>
         </tr>
       </thead>
       <tbody>
         {result.map((all, index) => {
-          if (all.countryRegion === country) {
-            return (
-              <tr key={index}>
-                <th>{all.provinceState}</th>
-                <th>{all.active}</th>
-                <th>{all.confirmed}</th>
-                <th>{all.recovered}</th>
-                <th>{all.deaths}</th>
-              </tr>
-            );
-          }
+          return (
+            <tr key={index}>
+              <th>
+                {all.provinceState} {all.admin2}
+              </th>
+              <th>{all.active}</th>
+              <th>{all.confirmed}</th>
+              <th>{all.recovered}</th>
+              <th>{all.deaths}</th>
+            </tr>
+          );
         })}
       </tbody>
     </table>
